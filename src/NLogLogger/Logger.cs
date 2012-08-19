d37a546d60f922b2148c5660d3b04fd94d33ec0c
Kenneth Choe <kenneth.choe@gmail.com>
2012-08-18 23:48:46 -0500

    packages folder relocated for github
    Github vendor.yml does not take src\packages as nuget. It expects
    packages folder to be at the root level.
    So moving packages folder location using a nuget patch
    http://nuget.org/packages/NuGetPackageFolderOverride.

---
+++ src/NLogLogger/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using JobQueueCore;
+using NLog;
+
+namespace NLogLogger
+{
+    public class Logger : ILoggerDelegate
+    {
+        public void Log(LogActivity activity, string subject)
+        {
+            PrepareLogger(subject).Info(activity.ToString());
+        }
+
+        public void LogError(string subject, Exception e)
+        {
+            PrepareLogger(subject).ErrorException(LogActivity.ErrorOccurred.ToString(), e);
+        }
+
+        public void LogDebugInfo(string subject, string debugInfo)
+        {
+            PrepareLogger(subject).Debug(debugInfo);
+        }
+
+        private NLog.Logger PrepareLogger(string subject)
+        {
+            var callerMethod = (new StackFrame(2, true).GetMethod());
+
+            string loggerName = "";
+            if (callerMethod.DeclaringType != null)
+            {
+                loggerName = callerMethod.DeclaringType.FullName;
+            }
+
+            var logger = LogManager.GetLogger(loggerName);
+            MappedDiagnosticsContext.Set("subject", subject);
+
+            return logger;
+        }
+    }
+}
